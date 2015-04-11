69ca460deeafafc8f010b01543e554d345236148
Gowri Shankar <gowrishankar_m@hotmail.com>
2015-04-11 23:11:08 +0530

    Matrix inversion cache function are added

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,63 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+# makeCacheMatrix function takes a 'matrix' object as input and
+# returns list of methods operated on the given matrix to cache
+# the inverted matrix and retrieve it. It also ensures if the matrix 
+# gets modified, inverted matrix is reset to null.
 
+# INPUT <- A Square Matrix object
+# OUTPUT <- A List Object holding functions to operate on the matrix and its invert
 makeCacheMatrix <- function(x = matrix()) {
 
+	inv_m <- NULL
+	set <- function(y) {
+		x <<- y
+        # When the matrix changes, ensuring inversion is reset
+		inv_m <<- NULL
+	}
+
+    # Getter method to acquire the original matrix
+	get <- function() x
+
+    # set and get inverted matrix.
+	set_inverse_matrix <- function(inv) inv_m <<- inv
+	get_inverse_matrix <- function() inv_m
+
+    # return the list of methods for inversion operation
+	list(set = set, get = get, 
+		set_inverse_matrix = set_inverse_matrix, 
+		get_inverse_matrix = get_inverse_matrix)
 }
 
 
-## Write a short comment describing this function
+# cacheSolve takes 'list' object created thru makeCacheMatrix function.
+# It checks for the inverted matrix, if it is computed already it returns.
+# If it is not computed, it computes and updates the cached matrix object.
 
+# INPUT <- A List Object holding functions to operate on the matrix and its invert
+# OUTPUT <- Inverted Matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+    # Get the inverted matrix
+	inv_m <- x$get_inverse_matrix()
+
+    # if it is already inverted, return the inverted one
+	if(!is.null(inv_m)) {
+		message("getting cached inverse matrix")
+		return(inv_m)
+	}
+    
+    # if it is not inverted, acquire the matrix and invert it
+	data <- x$get()
+
+    # Only a square matrix can be inverted. Ensure the given matrix is invertible
+	inv_m <- solve(data, ...)
+
+    # ensure the inverted matrix is cached
+    x$set_inverse_matrix(inv_m)
+
+    # return the inverted matrix
+	inv_m
+
 }
